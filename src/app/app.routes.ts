f75b9550c23ffabf1bfe77031e865c2903e07588
Alexandra <ac.alexandra25@gmail.com>
2020-02-11 17:57:56 +0100

    routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,4 @@
-import { RouterModule, Routes } from "@angular/router";
+import { RouterModule, Routes } from '@angular/router';
 
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { LoginComponent } from './login/login.component';
@@ -10,21 +10,11 @@ import { RegisterComponent } from './login/register/register.component';
 
 
 const appRoutes: Routes = [
-    { 
-        path: '', 
-        component: PagesComponent,
-        children: [
-            { path: 'dashboard', component: DashboardComponent },
-            { path: 'progress', component: ProgressComponent },
-            { path: 'graficas1', component: Graficas1Component },
-            { path: '', redirectTo:'/dashboard' , pathMatch: 'full' },
-        ]
-    },
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
 
-    
-    { path: '**', component: NopagefoundComponent }
-]
 
-export const APP_ROUTES = RouterModule.forRoot( appRoutes, { useHash: true } )
+    { path: '**', component: NopagefoundComponent }
+];
+
+export const APP_ROUTES = RouterModule.forRoot( appRoutes, { useHash: true } );
